8dabb4c2a732cb7ea4a955eebc18952a6007955e
Gabe Ehrlich <ehrlichg@wit.edu>
2022-10-14 23:52:56 -0400

    remove dead imports

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {firestore} from '../lib/firebase'
-import { doc, getDoc } from "firebase/firestore";
-import {useEffect, useState} from 'react'
 import Game from "../components/Game";
 
 const Home = () =>{
