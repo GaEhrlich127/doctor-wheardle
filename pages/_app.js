7d8f2a0de75270f8212be34871a4381a01d1df39
Gabe Ehrlich <ehrlichg@wit.edu>
2022-10-16 13:23:38 -0400

    testing a new search system

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div data-version='1.0.1'>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
