1b851d674345781d0291fc76a6af298eb149c5cc
Gabe Ehrlich <ehrlichg@wit.edu>
2022-10-20 23:55:12 -0400

    Randomize Banger order

--- components/BangersInfo.js
+++ components/BangersInfo.js
@@ -1,56 +1,108 @@
 const bangersInfo = 
 [
   {
-    "path": "Series 9/Same Old Day.mp3",
-    "name": "Same Old Day - Series 9"
+    "path": "The Doctor The Widow The Wardrobe & The Snowmen/Whose Engima.mp3",
+    "name": "Whose Engima - The Doctor The Widow The Wardrobe & The Snowmen"
   },
   {
-    "path": "Series 4/The Rueful Fate Of Donna Noble.mp3",
-    "name": "The Rueful Fate Of Donna Noble - Series 4"
+    "path": "Series 7/Gunslingers.mp3",
+    "name": "Gunslingers - Series 7"
   },
   {
-    "path": "Series 6/Time is Moving.mp3",
-    "name": "Time is Moving - Series 6"
+    "path": "Series 6/The Wedding of River Song.mp3",
+    "name": "The Wedding of River Song - Series 6"
   },
   {
-    "path": "Series 6/The Enigma Of River Song.mp3",
-    "name": "The Enigma Of River Song - Series 6"
+    "path": "Day of the Doctor & Time of the Doctor/It's Him (The Majestic Tale).mp3",
+    "name": "It's Him (The Majestic Tale) - Day of the Doctor & Time of the Doctor"
+  },
+  {
+    "path": "Series 4/UNIT Rocks.mp3",
+    "name": "UNIT Rocks - Series 4"
+  },
+  {
+    "path": "Series 4 Specials/The Time Lords' Last Stand.mp3",
+    "name": "The Time Lords' Last Stand - Series 4 Specials"
+  },
+  {
+    "path": "Series 6/Pop.mp3",
+    "name": "Pop - Series 6"
+  },
+  {
+    "path": "Series 4 Specials/The End Draws Near.mp3",
+    "name": "The End Draws Near - Series 4 Specials"
+  },
+  {
+    "path": "Series 8/Hello Hello.mp3",
+    "name": "Hello Hello - Series 8"
+  },
+  {
+    "path": "Series 7/The Long Song.mp3",
+    "name": "The Long Song - Series 7"
+  },
+  {
+    "path": "Series 7/Bah Bah Biker.mp3",
+    "name": "Bah Bah Biker - Series 7"
+  },
+  {
+    "path": "Series 6/1969.mp3",
+    "name": "1969 - Series 6"
+  },
+  {
+    "path": "Series 7/Up The Shard.mp3",
+    "name": "Up The Shard - Series 7"
+  },
+  {
+    "path": "Series 6/Tell Me Who You Are.mp3",
+    "name": "Tell Me Who You Are - Series 6"
+  },
+  {
+    "path": "Series 8/The Majestic Tale (of An Idiot With a Box).mp3",
+    "name": "The Majestic Tale (of An Idiot With a Box) - Series 8"
   },
   {
     "path": "Series 8/A Good Man; (Twelve's Theme).mp3",
     "name": "A Good Man? (Twelve's Theme) - Series 8"
   },
   {
+    "path": "Series 6/Brigadier Lethbridge-Stewart.mp3",
+    "name": "Brigadier Lethbridge - Stewart - Series 6"
+  },
+  {
     "path": "Day of the Doctor & Time of the Doctor/Two Doctors.mp3",
     "name": "Two Doctors - Day of the Doctor & Time of the Doctor"
   },
   {
-    "path": "Series 9/This is Not a War.mp3",
-    "name": "This is Not a War - Series 9"
+    "path": "Series 3/My Angel Put the Devil in Me.mp3",
+    "name": "My Angel Put the Devil in Me - Series 3"
   },
   {
-    "path": "Series 6/I Am The Doctor in Utah.mp3",
-    "name": "I Am The Doctor in Utah - Series 6"
+    "path": "Series 7/The Speeder.mp3",
+    "name": "The Speeder - Series 7"
   },
   {
-    "path": "Series 5/Words Win Wars.mp3",
-    "name": "Words Win Wars - Series 5"
+    "path": "Series 6/When A River Forms.mp3",
+    "name": "When A River Forms - Series 6"
   },
   {
-    "path": "Series 1 & 2/I'm Coming To Get You.mp3",
-    "name": "I'm Coming To Get You - Series 1 & 2"
+    "path": "Series 6/The Enigma Of River Song.mp3",
+    "name": "The Enigma Of River Song - Series 6"
   },
   {
-    "path": "Series 6/A Very Unusual Melody.mp3",
-    "name": "A Very Unusual Melody - Series 6"
+    "path": "Series 3/This Is Gallifrey; Our Childhood,.mp3",
+    "name": "This Is Gallifrey: Our Childhood, Our Home - Series 3"
   },
   {
-    "path": "Series 7/Infinite Potential.mp3",
-    "name": "Infinite Potential - Series 7"
+    "path": "Series 9/Waiting for the Veil.mp3",
+    "name": "Waiting for the Veil - Series 9"
   },
   {
-    "path": "Series 4 Specials/The Clouds Pass.mp3",
-    "name": "The Clouds Pass - Series 4 Specials"
+    "path": "Series 3/All the Strange, Strange Creature.mp3",
+    "name": "All the Strange, Strange Creatures (The Trailer Music) - Series 3"
+  },
+  {
+    "path": "Series 5/I Remember You.mp3",
+    "name": "I Remember You - Series 5"
   },
   {
     "path": "Day of the Doctor & Time of the Doctor/This Time There's Three Of Us (The Majestic Tale).mp3",
@@ -61,268 +113,216 @@ const bangersInfo =
     "name": "Song for Four / Home - Day of the Doctor & Time of the Doctor"
   },
   {
-    "path": "Series 3/This Is Gallifrey; Our Childhood,.mp3",
-    "name": "This Is Gallifrey: Our Childhood, Our Home - Series 3"
-  },
-  {
-    "path": "Series 9/Time To Do It Properly.mp3",
-    "name": "Time To Do It Properly - Series 9"
-  },
-  {
-    "path": "Series 5/I Am the Doctor.mp3",
-    "name": "I Am the Doctor - Series 5"
-  },
-  {
-    "path": "Series 4/The Dark And Endless Dalek Night.mp3",
-    "name": "The Dark And Endless Dalek Night - Series 4"
-  },
-  {
-    "path": "Series 6/Pop.mp3",
-    "name": "Pop - Series 6"
-  },
-  {
-    "path": "The Doctor The Widow The Wardrobe & The Snowmen/Clara In The TARDIS.mp3",
-    "name": "Clara In The TARDIS - The Doctor The Widow The Wardrobe & The Snowmen"
-  },
-  {
-    "path": "Day of the Doctor & Time of the Doctor/Hello Twelve.mp3",
-    "name": "Hello Twelve - Day of the Doctor & Time of the Doctor"
-  },
-  {
-    "path": "Series 5/I Remember You.mp3",
-    "name": "I Remember You - Series 5"
-  },
-  {
-    "path": "Series 7/The Speeder.mp3",
-    "name": "The Speeder - Series 7"
-  },
-  {
-    "path": "Series 7/Up The Shard.mp3",
-    "name": "Up The Shard - Series 7"
-  },
-  {
-    "path": "Series 4 Specials/Vale Decem.mp3",
-    "name": "Vale Decem - Series 4 Specials"
-  },
-  {
-    "path": "Series 3/The Carrionites Swarm.mp3",
-    "name": "The Carrionites Swarm - Series 3"
-  },
-  {
-    "path": "Series 6/Brigadier Lethbridge-Stewart.mp3",
-    "name": "Brigadier Lethbridge - Stewart - Series 6"
-  },
-  {
-    "path": "Series 7/Together Or Not At All - The Song Of Amy And Rory.mp3",
-    "name": "Together Or Not At All - The Song Of Amy And Rory - Series 7"
-  },
-  {
-    "path": "Series 7/To Save The Doctor.mp3",
-    "name": "To Save The Doctor - Series 7"
-  },
-  {
-    "path": "Series 7/A Secret He Will Take To His Grave.mp3",
-    "name": "A Secret He Will Take To His Grave - Series 7"
-  },
-  {
-    "path": "Series 9/The Bootstrap Paradox.mp3",
-    "name": "The Bootstrap Paradox - Series 9"
-  },
-  {
-    "path": "Series 4/The Doctor's Theme Series 4.mp3",
-    "name": "The Doctor's Theme Series 4 - Series 4"
-  },
-  {
-    "path": "Day of the Doctor & Time of the Doctor/Man And Wife.mp3",
-    "name": "Man And Wife - Day of the Doctor & Time of the Doctor"
-  },
-  {
-    "path": "Series 7/Goodbye Pond.mp3",
-    "name": "Goodbye Pond - Series 7"
-  },
-  {
-    "path": "Series 8/Hello Hello.mp3",
-    "name": "Hello Hello - Series 8"
-  },
-  {
-    "path": "Series 3/All the Strange, Strange Creature.mp3",
-    "name": "All the Strange, Strange Creatures (The Trailer Music) - Series 3"
-  },
-  {
-    "path": "Series 6/Melody Pond.mp3",
-    "name": "Melody Pond - Series 6"
-  },
-  {
-    "path": "Series 9/The Shepherd's Boy.mp3",
-    "name": "The Shepherd's Boy - Series 9"
-  },
-  {
     "path": "Series 4 Specials/Four Knocks.mp3",
     "name": "Four Knocks - Series 4 Specials"
   },
   {
-    "path": "Series 1 & 2/Doomsday.mp3",
-    "name": "Doomsday - Series 1 & 2"
-  },
-  {
-    "path": "Series 5/A Useful Striker.mp3",
-    "name": "A Useful Striker - Series 5"
-  },
-  {
-    "path": "Series 9/Waiting for the Veil.mp3",
-    "name": "Waiting for the Veil - Series 9"
-  },
-  {
-    "path": "Series 7/Cumbria 1207.mp3",
-    "name": "Cumbria 1207 - Series 7"
-  },
-  {
-    "path": "Day of the Doctor & Time of the Doctor/Trenzalore;The Long Song;I Am Information (Reprise).mp3",
-    "name": "Trenzalore/The Long Song/I Am Information (Reprise) - Day of the Doctor & Time of the Doctor"
-  },
-  {
-    "path": "Series 4/A Pressing Need To Save The World.mp3",
-    "name": "A Pressing Need To Save The World - Series 4"
-  },
-  {
-    "path": "Series 4 Specials/The End Draws Near.mp3",
-    "name": "The End Draws Near - Series 4 Specials"
+    "path": "Day of the Doctor & Time of the Doctor/This Is How It Ends.mp3",
+    "name": "This Is How It Ends - Day of the Doctor & Time of the Doctor"
   },
   {
     "path": "Series 4/Hanging On The Tablaphone.mp3",
     "name": "Hanging On The Tablaphone - Series 4"
   },
   {
-    "path": "Series 3/The Futurekind.mp3",
-    "name": "The Futurekind - Series 3"
+    "path": "Series 5/Words Win Wars.mp3",
+    "name": "Words Win Wars - Series 5"
   },
   {
-    "path": "Series 6/The Wedding of River Song.mp3",
-    "name": "The Wedding of River Song - Series 6"
+    "path": "Series 6/Time is Moving.mp3",
+    "name": "Time is Moving - Series 6"
   },
   {
-    "path": "Series 6/5;02pm.mp3",
-    "name": "5:02pm - Series 6"
+    "path": "Day of the Doctor & Time of the Doctor/Trenzalore;The Long Song;I Am Information (Reprise).mp3",
+    "name": "Trenzalore/The Long Song/I Am Information (Reprise) - Day of the Doctor & Time of the Doctor"
   },
   {
-    "path": "Series 4/The Greatest Story Never Told.mp3",
-    "name": "The Greatest Story Never Told - Series 4"
+    "path": "Series 7/Infinite Potential.mp3",
+    "name": "Infinite Potential - Series 7"
   },
   {
-    "path": "Series 3/My Angel Put the Devil in Me.mp3",
-    "name": "My Angel Put the Devil in Me - Series 3"
+    "path": "Series 4 Specials/The Clouds Pass.mp3",
+    "name": "The Clouds Pass - Series 4 Specials"
   },
   {
-    "path": "Series 1 & 2/Harriet Jones, Prime Minister.mp3",
-    "name": "Harriet Jones, Prime Minister - Series 1 & 2"
+    "path": "Series 7/Goodbye Pond.mp3",
+    "name": "Goodbye Pond - Series 7"
   },
   {
-    "path": "Series 4 Specials/Altering Lives.mp3",
-    "name": "Altering Lives - Series 4 Specials"
-  },
-  {
-    "path": "Day of the Doctor & Time of the Doctor/Never Tell Me The Rules.mp3",
-    "name": "Never Tell Me The Rules - Day of the Doctor & Time of the Doctor"
-  },
-  {
-    "path": "Series 3/The Dream of a Normal Death.mp3",
-    "name": "The Dream of a Normal Death - Series 3"
-  },
-  {
-    "path": "Series 3/Boe.mp3",
-    "name": "Boe - Series 3"
-  },
-  {
-    "path": "Series 5/Fish Custard.mp3",
-    "name": "Fish Custard - Series 5"
-  },
-  {
-    "path": "Day of the Doctor & Time of the Doctor/It's Him (The Majestic Tale).mp3",
-    "name": "It's Him (The Majestic Tale) - Day of the Doctor & Time of the Doctor"
-  },
-  {
-    "path": "Series 6/When A River Forms.mp3",
-    "name": "When A River Forms - Series 6"
-  },
-  {
-    "path": "Series 3/All the Strange, Strange Creatures (Remix).mp3",
-    "name": "All the Strange, Strange Creatures (The Trailer Music) [Remix] - Series 3"
-  },
-  {
-    "path": "Series 4 Specials/The Time Lords' Last Stand.mp3",
-    "name": "The Time Lords' Last Stand - Series 4 Specials"
-  },
-  {
-    "path": "Series 4/Doctor Who Opening Credits.mp3",
-    "name": "Doctor Who Opening Credits - Series 4"
-  },
-  {
-    "path": "Series 7/The Long Song.mp3",
-    "name": "The Long Song - Series 7"
-  },
-  {
-    "path": "Series 6/Tell Me Who You Are.mp3",
-    "name": "Tell Me Who You Are - Series 6"
-  },
-  {
-    "path": "Series 3/YANA (Excerpt).mp3",
-    "name": "YANA (Excerpt) - Series 3"
-  },
-  {
-    "path": "Series 7/Gunslingers.mp3",
-    "name": "Gunslingers - Series 7"
-  },
-  {
-    "path": "Series 5/Beneath Stonehenge.mp3",
-    "name": "Beneath Stonehenge - Series 5"
-  },
-  {
-    "path": "Series 9/The Woman He Loves.mp3",
-    "name": "The Woman He Loves - Series 9"
-  },
-  {
-    "path": "Series 4/UNIT Rocks.mp3",
-    "name": "UNIT Rocks - Series 4"
-  },
-  {
-    "path": "Series 1 & 2/Rose Defeats The Daleks.mp3",
-    "name": "Rose Defeats The Daleks - Series 1 & 2"
-  },
-  {
-    "path": "The Doctor The Widow The Wardrobe & The Snowmen/Whose Engima.mp3",
-    "name": "Whose Engima - The Doctor The Widow The Wardrobe & The Snowmen"
-  },
-  {
-    "path": "Series 7/Bah Bah Biker.mp3",
-    "name": "Bah Bah Biker - Series 7"
-  },
-  {
-    "path": "Series 6/Forgiven.mp3",
-    "name": "Forgiven - Series 6"
-  },
-  {
-    "path": "Series 6/1969.mp3",
-    "name": "1969 - Series 6"
-  },
-  {
-    "path": "Series 1 & 2/Hologram.mp3",
-    "name": "Hologram - Series 1 & 2"
+    "path": "Series 7/A Secret He Will Take To His Grave.mp3",
+    "name": "A Secret He Will Take To His Grave - Series 7"
   },
   {
     "path": "Series 6/The Majestic Tale (of a Madman in a Box).mp3",
     "name": "The Majestic Tale (of a Madman in a Box) - Series 6"
   },
   {
-    "path": "Series 8/The Majestic Tale (of An Idiot With a Box).mp3",
-    "name": "The Majestic Tale (of An Idiot With a Box) - Series 8"
+    "path": "Series 4/Doctor Who Opening Credits.mp3",
+    "name": "Doctor Who Opening Credits - Series 4"
+  },
+  {
+    "path": "Series 3/The Futurekind.mp3",
+    "name": "The Futurekind - Series 3"
+  },
+  {
+    "path": "Series 3/YANA (Excerpt).mp3",
+    "name": "YANA (Excerpt) - Series 3"
+  },
+  {
+    "path": "Series 9/This is Not a War.mp3",
+    "name": "This is Not a War - Series 9"
+  },
+  {
+    "path": "Series 3/Boe.mp3",
+    "name": "Boe - Series 3"
+  },
+  {
+    "path": "Series 7/To Save The Doctor.mp3",
+    "name": "To Save The Doctor - Series 7"
+  },
+  {
+    "path": "Series 9/The Bootstrap Paradox.mp3",
+    "name": "The Bootstrap Paradox - Series 9"
+  },
+  {
+    "path": "Series 6/Melody Pond.mp3",
+    "name": "Melody Pond - Series 6"
+  },
+  {
+    "path": "Series 4/The Dark And Endless Dalek Night.mp3",
+    "name": "The Dark And Endless Dalek Night - Series 4"
+  },
+  {
+    "path": "Series 5/Fish Custard.mp3",
+    "name": "Fish Custard - Series 5"
+  },
+  {
+    "path": "Series 1 & 2/I'm Coming To Get You.mp3",
+    "name": "I'm Coming To Get You - Series 1 & 2"
+  },
+  {
+    "path": "Series 6/I Am The Doctor in Utah.mp3",
+    "name": "I Am The Doctor in Utah - Series 6"
+  },
+  {
+    "path": "Series 6/5;02pm.mp3",
+    "name": "5:02pm - Series 6"
+  },
+  {
+    "path": "Series 4 Specials/Altering Lives.mp3",
+    "name": "Altering Lives - Series 4 Specials"
+  },
+  {
+    "path": "Series 3/All the Strange, Strange Creatures (Remix).mp3",
+    "name": "All the Strange, Strange Creatures (The Trailer Music) [Remix] - Series 3"
+  },
+  {
+    "path": "Series 4/A Pressing Need To Save The World.mp3",
+    "name": "A Pressing Need To Save The World - Series 4"
+  },
+  {
+    "path": "Series 7/Cumbria 1207.mp3",
+    "name": "Cumbria 1207 - Series 7"
+  },
+  {
+    "path": "Day of the Doctor & Time of the Doctor/Hello Twelve.mp3",
+    "name": "Hello Twelve - Day of the Doctor & Time of the Doctor"
+  },
+  {
+    "path": "Series 6/Forgiven.mp3",
+    "name": "Forgiven - Series 6"
+  },
+  {
+    "path": "Series 3/The Carrionites Swarm.mp3",
+    "name": "The Carrionites Swarm - Series 3"
+  },
+  {
+    "path": "Series 1 & 2/Rose Defeats The Daleks.mp3",
+    "name": "Rose Defeats The Daleks - Series 1 & 2"
+  },
+  {
+    "path": "Series 3/The Dream of a Normal Death.mp3",
+    "name": "The Dream of a Normal Death - Series 3"
+  },
+  {
+    "path": "Series 9/The Woman He Loves.mp3",
+    "name": "The Woman He Loves - Series 9"
+  },
+  {
+    "path": "The Doctor The Widow The Wardrobe & The Snowmen/Clara In The TARDIS.mp3",
+    "name": "Clara In The TARDIS - The Doctor The Widow The Wardrobe & The Snowmen"
+  },
+  {
+    "path": "Series 9/Time To Do It Properly.mp3",
+    "name": "Time To Do It Properly - Series 9"
+  },
+  {
+    "path": "Series 1 & 2/Doomsday.mp3",
+    "name": "Doomsday - Series 1 & 2"
+  },
+  {
+    "path": "Series 9/Same Old Day.mp3",
+    "name": "Same Old Day - Series 9"
+  },
+  {
+    "path": "Series 1 & 2/Harriet Jones, Prime Minister.mp3",
+    "name": "Harriet Jones, Prime Minister - Series 1 & 2"
+  },
+  {
+    "path": "Series 5/I Am the Doctor.mp3",
+    "name": "I Am the Doctor - Series 5"
+  },
+  {
+    "path": "Series 4/The Greatest Story Never Told.mp3",
+    "name": "The Greatest Story Never Told - Series 4"
+  },
+  {
+    "path": "Series 9/The Shepherd's Boy.mp3",
+    "name": "The Shepherd's Boy - Series 9"
+  },
+  {
+    "path": "Series 4/The Doctor's Theme Series 4.mp3",
+    "name": "The Doctor's Theme Series 4 - Series 4"
+  },
+  {
+    "path": "Series 6/A Very Unusual Melody.mp3",
+    "name": "A Very Unusual Melody - Series 6"
+  },
+  {
+    "path": "Series 1 & 2/Hologram.mp3",
+    "name": "Hologram - Series 1 & 2"
+  },
+  {
+    "path": "Series 4 Specials/Vale Decem.mp3",
+    "name": "Vale Decem - Series 4 Specials"
+  },
+  {
+    "path": "Series 4/The Rueful Fate Of Donna Noble.mp3",
+    "name": "The Rueful Fate Of Donna Noble - Series 4"
+  },
+  {
+    "path": "Series 5/Beneath Stonehenge.mp3",
+    "name": "Beneath Stonehenge - Series 5"
+  },
+  {
+    "path": "Day of the Doctor & Time of the Doctor/Man And Wife.mp3",
+    "name": "Man And Wife - Day of the Doctor & Time of the Doctor"
+  },
+  {
+    "path": "Day of the Doctor & Time of the Doctor/Never Tell Me The Rules.mp3",
+    "name": "Never Tell Me The Rules - Day of the Doctor & Time of the Doctor"
   },
   {
     "path": "The Doctor The Widow The Wardrobe & The Snowmen/One Word.mp3",
     "name": "One Word - The Doctor The Widow The Wardrobe & The Snowmen"
   },
   {
-    "path": "Day of the Doctor & Time of the Doctor/This Is How It Ends.mp3",
-    "name": "This Is How It Ends - Day of the Doctor & Time of the Doctor"
+    "path": "Series 5/A Useful Striker.mp3",
+    "name": "A Useful Striker - Series 5"
+  },
+  {
+    "path": "Series 7/Together Or Not At All - The Song Of Amy And Rory.mp3",
+    "name": "Together Or Not At All - The Song Of Amy And Rory - Series 7"
   }
 ]
 
